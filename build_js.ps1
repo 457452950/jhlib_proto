51fc7188ba342b32af547b6e9ff0fd8fa0632cf4
wlb <1602434826@qq.com>
2022-04-01 11:16:24 +0800

    update js builder

--- build_js.ps1
+++ build_js.ps1
@@ -1,5 +1,17 @@
 
-pbjs -t json Hi.proto > Hi.json
+pbjs -t json MessageBase.proto > MessageBase.json
+copy MessageBase.json MessageBase.js
 
-copy Hi.json Hi.js
+pbjs -t json DataBase.proto > DataBase.json
+copy DataBase.json DataBase.js
+
+pbjs -t json ReqAndRes.proto > ReqAndRes.json
+copy ReqAndRes.json ReqAndRes.js
+
+pbjs -t json Message.proto > Message.json
+copy Message.json Message.js
+
+
+
+pause
 
